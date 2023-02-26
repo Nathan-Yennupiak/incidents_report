580fd0ddea22349c8ee9a0ea5de086b1f02831a5
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2023-02-26 15:58:06 -0800

    Updated DBconnection

--- db.js
+++ db.js
@@ -1,28 +1,26 @@
 // LOCAL HOST CONNECTION
+require('dotenv').config();
 
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL
-const pool = new Pool({ connectionString });
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err ? err.stack : res.rows[0].now);
-  pool.end();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 
 //------------------------------------------------------------//
 
 // LIVE CONNECTION TO RENDER.. 
+// require('dotenv').config(); 
+
 // const { Pool } = require('pg');
 
 // const pool = new Pool({
-//   user: 'incidentsreport_user',
-//   host: 'dpg-cft3mg9a6gdotcd3mubg-a.oregon-postgres.render.com',
-//   database: 'incidentsreport',
-//   password: '0L2LpMMUFLrqiMHgyM8iI9oxSby13ffC',
-//   port: 5432,
+//   connectionString: process.env.LIVE_DATABASE_URL,
 // });
 
+
+// export the pool object
 module.exports = {
   query: (text, params) => pool.query(text, params)
 }
